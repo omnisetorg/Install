ac109ce3e29615f78cd3d16ea13e046310dbef18
MrGKanev <contact@mrgkanev.eu>
2025-01-17 16:12:07 +0200

    wip

--- install/development/dev-language.sh
+++ install/development/dev-language.sh
@@ -1,17 +1,13 @@
 # The way of doing was inspired by OmaKub. - https://github.com/basecamp/omakub/blob/master/install/terminal/select-dev-language.sh
 
 # Install default programming languages
-if AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
+if AVAILABLE_LANGUAGES=("Node.js" "Go" "PHP" "Python" "Ruby on Rails" "Elixir" "Rust" "Java")
 	languages=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --height 10 --header "Select programming languages")
 fi
 
 if [[ -n "$languages" ]]; then
 	for language in $languages; do
 		case $language in
-		Ruby)
-			mise use --global ruby@3.3
-			mise x ruby -- gem install rails --no-document
-			;;
 		Node.js)
 			mise use --global node@lts
 			;;
@@ -20,7 +16,7 @@ if [[ -n "$languages" ]]; then
 			;;
 		PHP)
 			sudo add-apt-repository -y ppa:ondrej/php
-			sudo apt -y install php8.3 php8.3-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+			sudo apt -y install php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
 			php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 			php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
 			rm composer-setup.php
@@ -32,6 +28,10 @@ if [[ -n "$languages" ]]; then
 			mise use --global erlang@latest
 			mise use --global elixir@latest
 			mise x elixir -- mix local.hex --force
+			;;
+ 		Ruby)
+			mise use --global ruby@3.3
+			mise x ruby -- gem install rails --no-document
 			;;
 		Rust)
 			bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)" -- -y
